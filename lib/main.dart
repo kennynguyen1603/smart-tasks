5767249cd32b0872c1008056e279f6f98785224f
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-07-02 22:37:39 +0700

    Refactor main.dart to integrate ProviderScope and use MaterialApp.router for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:smart_tasks/features/auth/presentation/views/login_page.dart';
+import 'package:smart_tasks/core/routes/app_router.dart';
 import 'package:smart_tasks/shared/services/shared_prefs_service.dart';
 import 'firebase_options.dart';
 
@@ -9,7 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefsService.init();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
@@ -18,10 +18,11 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // final isDarkMode = ref.watch(sharedPrefsServiceProvider).isDarkMode;
-    return MaterialApp(
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      // theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: const LoginPage(),
+      title: 'SmartTasks',
+      routerConfig: router,
     );
   }
 }
