d6ec0db4f49c0c1e6ec4047f052253524c759068
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-07-02 22:37:25 +0700

    Add HomePage widget for main application interface

---
+++ lib/features/home/presentation/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smart_tasks/features/auth/presentation/viewmodels/auth_view_model.dart';
+import '../../../profile/presentation/pages/profile_page.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SmartTasks'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              if (user != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => ProfilePage(user: user)),
+                );
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await ref.read(authViewModelProvider.notifier).signOut();
+              if (context.mounted) {
+                context.go('/login');
+              }
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to SmartTasks!\nLet’s get organized.',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/add_task');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
